e9bfdfe2afa1b649331f3891409d68d8d4f63b25
puggythemeddler <puggythemeddler@users.noreply.github.com>
2026-07-29 16:34:27 +0300

    Set NODE_ENV=development during client backend build to include devDeps

--- control-plane/server/provision.ts
+++ control-plane/server/provision.ts
@@ -137,7 +137,7 @@ async function createRenderService(clientName: string, dbUrl: string, clientSlug
       serviceDetails: {
         runtime: "node",
         envSpecificDetails: {
-          buildCommand: "npm ci && cd server && npx tsc && cd ..",
+          buildCommand: "NODE_ENV=development npm ci && cd server && npx tsc && cd ..",
           startCommand: "node server/dist/index.js",
         },
         plan: "free",
